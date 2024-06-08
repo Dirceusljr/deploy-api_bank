629b5f74d40b2276e43278dc94f23fc562e3f93c
dirceusljr <dirceusljr@gmail.com>
2024-06-07 21:10:06 -0300

    criando endpoint conta

--- src/app.js
+++ src/app.js
@@ -1,9 +1,19 @@
 import express from 'express';
 
 const app = express();
+app.use(express.json());
 
 app.get('/', (req, res) => {
     res.send('Hello World');
+})
+
+const conta = [{
+    "conta_id": 1234,
+     "saldo": 200
+}]
+
+app.get('/conta', (req, res) => {
+    res.status(200).json(conta)
 })
 
 export default app;
