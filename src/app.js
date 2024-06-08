9c2808dba238d7ccbafb191c43d86624d1e703a9
dirceusljr <dirceusljr@gmail.com>
2024-06-07 21:18:40 -0300

    criando endpoint transacao

--- src/app.js
+++ src/app.js
@@ -3,17 +3,28 @@ import express from 'express';
 const app = express();
 app.use(express.json());
 
-app.get('/', (req, res) => {
-    res.send('Hello World');
-})
-
 const conta = [{
     "conta_id": 1234,
      "saldo": 200
 }]
 
+const transacao = []
+
+app.get('/', (req, res) => {
+    res.send('Hello World');
+})
+
+
 app.get('/conta', (req, res) => {
     res.status(200).json(conta)
+})
+
+app.post('/transacao', (req, res) => {
+    const novaTransacao = req.body
+    res.status(201).json({
+        message: "Transação realizada com sucesso",
+        transacao: novaTransacao
+    })
 })
 
 export default app;
